2abf7f1126da73d51395cafa86bb30a9b87ac474
ClintPy <cmaruti93@gmail.com>
2019-12-12 23:43:43 +0300

    Added Client

--- src/server.js
+++ src/server.js
@@ -22,7 +22,7 @@ app.get('/', (req,res) => {
 app.use(userRouter)
 app.use(tasksRouter)
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 
 app.listen(port, () => console.log('server started'))
 
