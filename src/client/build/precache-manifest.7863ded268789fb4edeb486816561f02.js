725c47a30e4c076dab8127d8a969da072a720059
Hunter <hunterrwaters@gmail.com>
2020-06-01 17:46:37 -0700

    Adding build to source control

---
+++ src/client/build/precache-manifest.7863ded268789fb4edeb486816561f02.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c13e6f8e4996348d8b1d48da829f8bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5dacaf1fe9884add9748",
+    "url": "/static/css/main.f050f7af.chunk.css"
+  },
+  {
+    "revision": "c8bcb315d5e1fe8486bf",
+    "url": "/static/js/2.97fcb8b3.chunk.js"
+  },
+  {
+    "revision": "5dacaf1fe9884add9748",
+    "url": "/static/js/main.84a37c83.chunk.js"
+  },
+  {
+    "revision": "5b06b92bf662c1e82036",
+    "url": "/static/js/runtime~main.5cda6740.js"
+  }
+]);
